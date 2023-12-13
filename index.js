7110798b5934fb74b19516573f76fcc4082dcbe7
Alistair Pattison <alistairpattison@gmail.com>
2023-12-12 22:53:21 -0600

    minimum viable product

---
+++ index.js
@@ -0,0 +1,26 @@
+import { cpSync, mkdirSync, readFileSync, writeFileSync } from "fs";
+import { spawnSync } from "child_process";
+import yaml from "js-yaml";
+import hbs from "handlebars";
+
+const OUTPUT_DIR = ".out";
+const TEMPLATES_DIR = "templates";
+
+const buildTex = (cv_content) => {
+  const template = readFileSync(`${TEMPLATES_DIR}/tex/main.tex.hbs`).toString();
+  const latex = hbs.compile(template)(cv_content);
+
+  // build latex
+  mkdirSync(`${OUTPUT_DIR}/tex`, { recursive: true });
+  writeFileSync(`${OUTPUT_DIR}/tex/main.tex`, latex);
+  cpSync(`${TEMPLATES_DIR}/tex/resume.cls`, `${OUTPUT_DIR}/tex/resume.cls`);
+  spawnSync("latexmk", ["-cd", `${OUTPUT_DIR}/tex/main.tex`]);
+};
+
+const main = () => {
+  const cv_content = yaml.load(readFileSync("main.yml"));
+
+  buildTex(cv_content);
+};
+
+main();
