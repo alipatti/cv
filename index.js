122594afa67190ce2302c13caf01c2ed46ea7be2
Alistair Pattison <alistairpattison@gmail.com>
2023-12-16 12:53:00 -0600

    added ability to render different subsets as separate pdfs

--- index.js
+++ index.js
@@ -1,31 +1,84 @@
-import { cpSync, mkdirSync, readFileSync, writeFileSync } from "fs";
-import { spawnSync } from "child_process";
+import { readFile, copyFile, writeFile, mkdir } from "fs/promises";
 import yaml from "js-yaml";
 import hbs from "handlebars";
+import Spinnies from "spinnies";
+import { promisify } from "util";
+import { exec as execCallback } from "child_process";
 
-const OUTPUT_DIR = ".out";
+const exec = promisify(execCallback);
+
+const OUT_DIR = ".out";
+const TEMP_DIR = ".temp";
 const TEMPLATES_DIR = "templates";
+const CV_BASE_NAME = "pattison-cv";
 
-const buildTex = (cv_content) => {
-  const template = readFileSync(`${TEMPLATES_DIR}/main.tex.hbs`).toString();
-  const latex = hbs.compile(template)(cv_content);
-
-  // build latex
-  mkdirSync(`${OUTPUT_DIR}/tex`, { recursive: true });
-  writeFileSync(`${OUTPUT_DIR}/tex/main.tex`, latex);
-  cpSync("publications.bib", `${OUTPUT_DIR}/tex/publications.bib`);
-  const process = spawnSync("latexmk", ["-cd", `${OUTPUT_DIR}/tex/main.tex`]);
-
-  if (process.status != 0) {
-    console.log(process.stdout.toString());
-    throw "LaTeX error";
-  }
+const getContent = async (tag) => {
+  const content = yaml.load(await readFile("main.yml"));
+  if (!tag || tag === "full") return content;
+  return filterContent(content, tag);
 };
 
-const main = () => {
-  const content = yaml.load(readFileSync("main.yml"));
+const getTags = async () =>
+  (await getContent()).sections
+    .map((section) => section.items?.map((item) => item.tags)) // find all tags in yaml
+    .flat(2) // flatten
+    .filter((tag, i, array) => array.indexOf(tag) === i) // remove dups
+    .filter((val) => val !== undefined); // remove undefined
 
-  buildTex(content);
+const filterSections = (sections, tag) =>
+  sections
+    // include only tagged items
+    .map(({ items, ...rest }) => ({
+      items: items?.filter((item) => item.tags?.includes(tag)),
+      ...rest,
+    }))
+    // remove sections with no items
+    .filter((section) => section.items?.length > 0);
+
+const filterContent = (content, tag) => ({
+  about: content.about,
+  sections: filterSections(content.sections, tag),
+});
+
+const buildTex = async (tag) => {
+  const content = await getContent(tag);
+
+  // generate tex source
+  const template = (await readFile(`${TEMPLATES_DIR}/main.tex.hbs`)).toString();
+  const latex = hbs.compile(template)(content);
+
+  // setup file structure
+  const workingDir = `${TEMP_DIR}/${tag}`;
+  const fileNameRoot = `${CV_BASE_NAME}-${tag}`;
+  await mkdir(`${workingDir}`, { recursive: true });
+  await writeFile(`${workingDir}/${fileNameRoot}.tex`, latex);
+  await copyFile("publications.bib", `${workingDir}/publications.bib`);
+
+  // build pdf
+  await exec(`latexmk -cd ${workingDir}/${fileNameRoot}.tex`);
+
+  // move pdf to output directory
+  await mkdir(`${OUT_DIR}`, { recursive: true });
+  await copyFile(
+    `${workingDir}/${fileNameRoot}.pdf`,
+    `${OUT_DIR}/${fileNameRoot}.pdf`
+  );
 };
 
-main();
+const main = async () => {
+
+  const tags = await getTags();
+  tags.push("full");
+
+  const spinnies = new Spinnies();
+
+  await Promise.all(
+    tags.map(async (tag) => {
+      spinnies.add(tag);
+      await buildTex(tag);
+      spinnies.succeed(tag);
+    })
+  );
+};
+
+await main();
